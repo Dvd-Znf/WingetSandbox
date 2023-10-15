ba72fce67eebdba38e424b044b67430f744e499e
Dvd-Znf <david.zanfir.trove@gmail.com>
2023-10-15 15:31:31 +0300

    Initial Commit!

---
+++ LogonScript.ps1
@@ -0,0 +1,18 @@
+# Winget is not installed by default in sandbox
+
+# Install winget:
+$progressPreference = 'silentlyContinue'
+$latestWingetMsixBundleUri = $(Invoke-RestMethod https://api.github.com/repos/microsoft/winget-cli/releases/latest).assets.browser_download_url | Where-Object {$_.EndsWith(".msixbundle")}
+$latestWingetMsixBundle = $latestWingetMsixBundleUri.Split("/")[-1]
+Write-Information "Downloading winget to artifacts directory..."
+Invoke-WebRequest -Uri $latestWingetMsixBundleUri -OutFile "./$latestWingetMsixBundle"
+Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-AppxPackage $latestWingetMsixBundle
+
+# Enable local manifests:
+winget settings --enable LocalManifestFiles
+
+
+# Exit the terminal, add this if you want
+# taskkill /IM powershell.exe
---
+++ README.md
@@ -0,0 +1,15 @@
+# Winget available automatically in WindowsSandbox
+If you're a [winget-pkgs](https://github.com/microsoft/winget-pkgs/) maintainer you know just how important testing your manifests is      
+This repo provides Windows Sandbox configuration and logon script if you want to manually test and verfiry any manifest/packages     
+   
+![Banner](/res/Banner.bmp)  
+
+## Why manualy verify like this when you can use an autotest script?
+I made this repo because I needed to manually check some packages and while I like automation sometimes less is more.  
+## How to use?
+```
+PS: cd Desktop
+PS: git clone   
+PS: WindowsSandbox WingetSandbox\WingetSanbox.wsb  
+```
+    
